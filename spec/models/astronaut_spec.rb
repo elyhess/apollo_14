6a9a4d3d92a3eb49f3658fee07a795d5fda9e85a
Ely Hess <ely.hess@me.com>
2020-12-21 15:42:17 -0700

    Add total_time_in_space test

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 describe Astronaut, type: :model do
+  before :each do
+    @astro1 = Astronaut.create!(name: "Bob", age: 30, job: "Commander")
+    @astro2 = Astronaut.create!(name: "Jill", age: 40, job: "Private")
+    @astro3 = Astronaut.create!(name: "Joe", age: 35, job: "Colonel")
+    @astro4 = Astronaut.create!(name: "Has", age: 32, job: "Lieutenant")
+
+    @mission1 = Mission.create!(title: "Mission to Mars", time_in_space: 20)
+    @mission2 = Mission.create!(title: "Mission to Moon", time_in_space: 1)
+    @mission3 = Mission.create!(title: "Mission to Sun", time_in_space: 80)
+
+    @w = AstronautMission.create!(astronaut: @astro1, mission: @mission1)
+    @x = AstronautMission.create!(astronaut: @astro1, mission: @mission2)
+    @y = AstronautMission.create!(astronaut: @astro2, mission: @mission3)
+    @z = AstronautMission.create!(astronaut: @astro3, mission: @mission3)
+  end
+
   describe 'Validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :age }
@@ -13,24 +29,15 @@ describe Astronaut, type: :model do
   end
 
   describe 'Class Methods' do
-    before :each do
-      @astro1 = Astronaut.create!(name: "Bob", age: 30, job: "Commander")
-      @astro2 = Astronaut.create!(name: "Jill", age: 40, job: "Private")
-      @astro3 = Astronaut.create!(name: "Joe", age: 35, job: "Colonel")
-      @astro4 = Astronaut.create!(name: "Has", age: 32, job: "Lieutenant")
-
-      @mission1 = Mission.create!(title: "Mission to Mars", time_in_space: 20)
-      @mission2 = Mission.create!(title: "Mission to Moon", time_in_space: 1)
-      @mission3 = Mission.create!(title: "Mission to Sun", time_in_space: 80)
-
-      @w = AstronautMission.create!(astronaut: @astro1, mission: @mission1)
-      @x = AstronautMission.create!(astronaut: @astro1, mission: @mission2)
-      @y = AstronautMission.create!(astronaut: @astro2, mission: @mission3)
-      @z = AstronautMission.create!(astronaut: @astro3, mission: @mission3)
-    end
-
     it '::average_age' do
       expect(Astronaut.average_age).to eq(34.25)
+    end
+  end
+
+  describe 'Instance Methods' do
+    it '#total_time_in_space' do
+      expect(@astro1.total_time_in_space).to eq(21)
+      expect(@astro2.total_time_in_space).to eq(80)
     end
   end
 end
