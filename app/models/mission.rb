fcb8aeba087360c3807dc8b505273ffbd1814bbd
Ely Hess <ely.hess@me.com>
2020-12-21 15:16:36 -0700

    Add sort_alphabetically

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -2,4 +2,8 @@ class Mission < ApplicationRecord
   validates_presence_of :title, :time_in_space
   has_many :astronaut_missions
   has_many :astronauts, through: :astronaut_missions
+
+  def self.sort_alphabetically
+    order(:title)
+  end
 end
